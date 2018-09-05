77684f45ce5175fd01c14135bac4828418e3927d
Guido Chia <chiaguido@gmail.com>
2018-09-05 09:01:41 -0300

    Resubo la clase mapa rediseñada, ahora sin matriz.

--- JuegoProyecto/ProyectoTDP/src/Mapas/Mapa1.java
+++ JuegoProyecto/ProyectoTDP/src/Mapas/Mapa1.java
@@ -1,5 +1,17 @@
 package Mapas;
 
 public class Mapa1 extends Mapa {
-
+	public Mapa1() {
+		super();
+		entidades= new LinkedList<Entidad>();
+		for (int i=0; i<20; i++) {
+			
+		}
+		for (int i=0; i<10; i++) {
+			
+		}
+		for (int i=0; i<4; i++) {
+			
+		}
+	}
 }
